050fa24966a3492b62e6ebc829d5d08fd0acf8b3
Kaitlin Perkins <kate.perkins2@gmail.com>
2020-03-23 14:48:59 -0600

    Add code example file to project.

---
+++ 23Mar20_CodeExample.R
@@ -0,0 +1,33 @@
+# This is an example to practice using version control (Github) by creating a data set and analyzing it.
+
+# Order of events:
+
+# I. Create data set
+# II. Produce plot of data set
+# III. Commit changes 
+# IV. Push to Github
+
+
+# I. Create data set
+
+x <- c(1,4,8,12)
+y <- c(2,8,16,24)
+
+
+# II. Produce a plot of the data set
+
+plot(x,y)
+
+
+# III. Commit changes to Github
+
+# Press the Version Control button (green, red, and gray) from the toolbar
+# Press commit
+# Select the box(es) of the files you'd like to stage
+# Write a comment about your edits
+
+
+# IV. Pull most recent document from Github
+
+# After you pull the most recent changes, push yours
+
